0712926a88356f8c2b820dfacc94b962c4fe83ea
unknown <Administrator@p5212-rpasystem.jp.pegasus-sewing.com>
2019-09-09 13:12:50 +0900

    fg_color項目追加。MACアドレスを表示。

--- run_on_console.py
+++ run_on_console.py
@@ -26,32 +26,38 @@ class BeaconManager():
         'RED': {
             'uuid': '0FEC7C6F-F05D-49AF-956A-CA08B413839F', # UUID is for macOS
             'mac_address': 'AC:23:3F:26:45:15', # MAC Adress is for Windows10
-            'rssi_data': []
+            'rssi_data': [],
+            'fg_color': 'red'
         },
         'BLUE': {
             'uuid': 'FE1F23B9-6794-437E-B8C9-7A9D31616636',
             'mac_address': 'AC:23:3F:26:40:5B',
-            'rssi_data': []
+            'rssi_data': [],
+            'fg_color': 'blue'
         },
         'YELLOW': {
             'uuid': '8F16E228-2BA6-4440-8CC4-6BD300EB5FB3',
             'mac_address': 'AC:23:3F:26:40:48',
-            'rssi_data': []
+            'rssi_data': [],
+            'fg_color': 'gold'
         },
         'GREEN': {
             'uuid': '63A5C056-D205-4248-97BA-1B23312B6392',
             'mac_address': 'AC:23:3F:26:40:40',
-            'rssi_data': []
+            'rssi_data': [],
+            'fg_color': 'green'
         },
         'PURPLE': {
             'uuid': 'C085CB8E-5731-4BB4-B36B-839B2F590CD1',
             'mac_address': 'AC:23:3F:26:45:C2',
-            'rssi_data': []
+            'rssi_data': [],
+            'fg_color': 'purple'
         },
-        'GRAY': {
+        'WHITE': {
             'uuid': 'CDF00AB3-6D0F-4B96-90CB-8BB1D4B43F99',
             'mac_address': 'AC:23:3F:26:42:EB',
-            'rssi_data': []
+            'rssi_data': [],
+            'fg_color': 'gray'
         }
     }
 
@@ -128,6 +134,7 @@ class BeaconManager():
             if len(beacon_dict) == 0 or beacon_dict.get('average_rssi') < average_rssi:
                 beacon_dict = {
                     'key_color': key_color,
+                    'fg_color': cls.beacons.get(key_color).get('fg_color'),
                     'average_rssi': average_rssi
                 }
         return beacon_dict
@@ -148,7 +155,7 @@ async def run_getting_packets(master=None):
     '''Bluetoothモジュールを使いアドバタイズパケットを取得する'''
     count = 0
     while True:
-        not_exists_color = ['RED', 'BLUE', 'YELLOW', 'GREEN', 'PURPLE', 'GRAY']
+        not_exists_color = ['RED', 'BLUE', 'YELLOW', 'GREEN', 'PURPLE', 'WHITE']
         devices = await discover()
         count += 1
         for d in devices:
@@ -190,11 +197,11 @@ async def run_getting_packets(master=None):
                         1.0,
                         'seq: ' + str(count) + ', rssi: ' + str(d.rssi) + 'dBm, average: ' + str(BeaconManager.get_average('PURPLE')) + 'dBm\n'
                     )
-                elif key_color == 'GRAY':
-                    BeaconManager.insert_rssi('GRAY', d.rssi)
+                elif key_color == 'WHITE':
+                    BeaconManager.insert_rssi('WHITE', d.rssi)
                     master.text6.insert(
                         1.0,
-                        'seq: ' + str(count) + ', rssi: ' + str(d.rssi) + 'dBm, average: ' + str(BeaconManager.get_average('GRAY')) + 'dBm\n'
+                        'seq: ' + str(count) + ', rssi: ' + str(d.rssi) + 'dBm, average: ' + str(BeaconManager.get_average('WHITE')) + 'dBm\n'
                     )    
                 not_exists_color.remove(key_color)
 
@@ -206,7 +213,7 @@ async def run_getting_packets(master=None):
         beacon_dic = BeaconManager.get_nearest_beacon()
         if beacon_dic is not None and len(beacon_dic) > 0:
             master.nearest_beacon_stringvar.set('The nearest beacon is the color of ' + beacon_dic.get('key_color') + ' (rssi: ' + str(beacon_dic.get('average_rssi')) + ')')
-            master.nearest_beacon['fg'] = beacon_dic.get('key_color').lower()
+            master.nearest_beacon['fg'] = beacon_dic.get('fg_color')
         else:
             master.nearest_beacon_stringvar.set('The nearest beacon is nothing.')
             master.nearest_beacon['fg'] = 'black'
@@ -230,42 +237,42 @@ class MainFrame(tk.Frame):
         self.master.after(0, self.fix)
 
     def create_widgets(self):
-        self.label1 = tk.Label(text='Beacon RED', fg='red')
+        self.label1 = tk.Label(text='Beacon RED (AC:23:3F:26:45:15)', fg='red')
         self.label1.grid(row=0)
         self.label1['pady'] = 5
         self.text1 = tk.Text(width=50, height=10)
         self.text1.grid(row=1)
         self.text1['pady'] = 5
 
-        self.label2 = tk.Label(text='Beacon BLUE', fg='blue')
+        self.label2 = tk.Label(text='Beacon BLUE (AC:23:3F:26:40:5B)', fg='blue')
         self.label2.grid(row=2)
         self.label2['pady'] = 5
         self.text2 = tk.Text(width=50, height=10)
         self.text2.grid(row=3)
         self.text2['pady'] = 5
 
-        self.label3 = tk.Label(text='Beacon YELLOW', fg='yellow')
+        self.label3 = tk.Label(text='Beacon YELLOW (AC:23:3F:26:40:48)', fg='gold')
         self.label3.grid(row=4)
         self.label3['pady'] = 5
         self.text3 = tk.Text(width=50, height=10)
         self.text3.grid(row=5)
         self.text3['pady'] = 5
 
-        self.label4 = tk.Label(text='Beacon GREEN', fg='green')
+        self.label4 = tk.Label(text='Beacon GREEN (AC:23:3F:26:40:40)', fg='green')
         self.label4.grid(row=0, column=1)
         self.label4['pady'] = 5
         self.text4 = tk.Text(width=50, height=10)
         self.text4.grid(row=1, column=1)
         self.text4['pady'] = 5
 
-        self.label5 = tk.Label(text='Beacon PURPLE', fg='purple')
+        self.label5 = tk.Label(text='Beacon PURPLE (AC:23:3F:26:45:C2)', fg='purple')
         self.label5.grid(row=2, column=1)
         self.label5['pady'] = 5
         self.text5 = tk.Text(width=50, height=10)
         self.text5.grid(row=3, column=1)
         self.text5['pady'] = 5
 
-        self.label6 = tk.Label(text='Beacon GRAY', fg='gray')
+        self.label6 = tk.Label(text='Beacon WHITE (AC:23:3F:26:42:EB)', fg='gray')
         self.label6.grid(row=4, column=1)
         self.label6['pady'] = 5
         self.text6 = tk.Text(width=50, height=10)
